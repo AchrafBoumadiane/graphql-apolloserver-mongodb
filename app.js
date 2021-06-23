d94032ccedeaae7fadd1e0bfd111f485cea9ed71
Achraf Boumadiane <achraf.boumadiane@yahoo.com>
2021-06-23 10:24:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const resolvers = require("./resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
+// The `listen` method launches a web server
 server.listen().then(({ url }) => {
 	console.log(`🚀  Server ready at ${url}`);
 });
